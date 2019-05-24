b7999cc488f649f0e38ac83078d9f8d4661ec827
xjzhandsome <48749083+xjzhandsome@users.noreply.github.com>
2019-05-24 21:27:00 +0800

    Update HelloNumbers.java

--- lab1/HelloNumbers.java
+++ lab1/HelloNumbers.java
@@ -3,9 +3,11 @@ class HelloNumbers
 	public static void main(String[] args)
 	{
 		int x;
+		int sum = 0;
 		for(x=0;x<10;x++)
-		{
-			System.out.print(x + " ");
+		{	
+			sum = sum + x;
+			System.out.print(sum + " ");
 		}
 		
 	}
